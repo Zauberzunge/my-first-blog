1106669da91ed109b35279d967474f2b5c986969
Zauberzunge <haraldwoerl@mail.de>
2020-12-13 13:34:34 +0100

    Signed-off-by: Zauberzunge <haraldwoerl@mail.de>

--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,4 @@ from .models import Post
 def post_list(request):
 	#posts = Post.objects.filter(published_date=timezone.now()).order_by('published_date')
 	posts = Post.objects.all()
-	return render(request, 'blog/post_list.html', {})
+	return render(request, 'blog/post_list.html', {'posts': posts})
