b4d3590cf295d82b8aca78c550a223db189b7a40
Zauberzunge <haraldwoerl@mail.de>
2020-12-13 13:29:20 +0100

    Signed-off-by: Zauberzunge <haraldwoerl@mail.de>

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,6 @@ from django.utils import timezone
 from .models import Post
 
 def post_list(request):
-	posts = Post.objects.filter(published_date<timezone.now()).order_by('published_date')
+	posts = Post.objects.filter(published_date=timezone.now()).order_by('published_date')
 	#posts = Post.objects()
 	return render(request, 'blog/post_list.html', {})
